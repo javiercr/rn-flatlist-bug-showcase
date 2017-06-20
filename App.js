ca7d03e1ae77e7980ec674b60479863950071a8a
Javier Cuevas <javi@diacode.com>
2017-06-20 12:04:28 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const data = []
+for (var index = 0; index < 500; index++) {
+  data.push({key: index})
+}
+
+export default class App extends React.Component {
+
+  onViewableItemsChanged = ({ viewableItems, changed }) => {
+    console.log("####  viewableItems #### ");
+    console.log(viewableItems.map((item) => item.key));
+    console.log("#### changed #### ");
+    changed.map((item) => console.log(`[${item.key}] isViewable = ${item.isViewable}`));
+  };
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={data}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+          style={styles.list}
+          onViewableItemsChanged={this.onViewableItemsChanged}
+          initialNumToRender={3}
+          windowSize={5}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+  },
+  item: {
+    fontSize: 200,
+    flex: 1,
+    width: '100%',
+    textAlign: 'center',
+    backgroundColor: '#ccc',
+    marginVertical: 10
+  }
+});
---
+++ README.md
@@ -0,0 +1,11 @@
+Demo project to showcase bug with `FlatList` / `VirtualizedList` and the `onViewableItemsChanged` callback on react-native `0.45.1`
+
+## Steps to reproduce:
+* Clone the project
+* yarn install
+* yarn run ios
+* Enable Remote Debugging (cmd + D)
+* Open console on debugger 
+* Scroll the list and notice how items that are visible are included on `changed` for the `onViewableItemsChanged` callback with `isViewable` as false.
+
+You can also notice that this bug is not present if you downgrade to ``react": "16.0.0-alpha.6"` and `"react-native": "^0.44.0"`.
